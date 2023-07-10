ec7342fc3864804f2712923dd2cae6e63cd1d9aa
reginaldoalvarez <reginaldo.alvarez@gmail.com>
2023-07-10 15:17:41 -0500

    Terminar Book It y mejora al obetener todas cities

--- tests/ingresoDatos.ts
+++ tests/ingresoDatos.ts
@@ -1,24 +1,53 @@
 //ingresoDatos.ts
 export async function dataIN(page: any) {
     // ... código de la función navigateAndFilterHotels ...
-    const allcities: string[] = [];// todas las location existentes en Dropdown
-  let city: string;  //Location
-  //Agregar nueva city
-  let addcity = true;
-  while (addcity === true) {
-    await page.click('label:has-text("Location")');
-    await page.press('body', 'ArrowDown');
-    city = ((await page.getByLabel('location').inputValue()));
 
-    if (!allcities.includes(city)) {
-      allcities.push(city);
-      addcity = true;
-    } else {
-      addcity = false;
+
+let city: string; // Location
+let addcity = true;
+await page.click('label:has-text("Location")');
+await page.locator('#MainContentPlaceHolder_SearchPanel_SearchPanelLayout_LocationComboBox_B-1').click()
+//await page.press('body', 'ArrowDown');
+const LocatorAllCities = '#MainContentPlaceHolder_SearchPanel_SearchPanelLayout_LocationComboBox_DDD_L_LBT.dxeListBoxItemRow_Metropolis'
+
+//await page.waitForSelector(LocatorAllCities)
+await page.waitForLoadState('load')
+
+const allcities: { city: string; country: string; numHoteles: string }[] = [];
+
+const tableElement = await page.$('#MainContentPlaceHolder_SearchPanel_SearchPanelLayout_LocationComboBox_DDD_L_LBT');
+const rows = await tableElement?.$$('tr.dxeListBoxItemRow_Metropolis');
+
+if (rows) {
+  for (const row of rows) {
+    const cityElement = await row.$('td.dxeFTM');
+    const countryElement = await row.$('td.dxeTM');
+    const numHotelesElement = await row.$('td.dxeLTM');
+
+    const city = await cityElement?.innerText();
+    const country = await countryElement?.innerText();
+    const numHoteles = await numHotelesElement?.innerText();
+
+    if (city && country && numHoteles) {
+      allcities.push({ city, country, numHoteles });
     }
-
-
   }
+}
+
+/* while (addcity) {
+  await page.click('label:has-text("Location")');
+  await page.press('body', 'ArrowDown');
+  city = await page.getByLabel('location').inputValue();
+  
+  const numHoteles = await page.locator('your-locator-for-counting-hotels').count().then(count => count.toString());
+
+  if (!allcities.some(item => item.city === city)) {
+    allcities.push({ city, numHoteles });
+    addcity = true;
+  } else {
+    addcity = false;
+  }
+} */
 
   console.log('El total de Locaciones seleccionables son: ' + allcities.length);
   console.log(allcities);
@@ -26,11 +55,13 @@ export async function dataIN(page: any) {
 
   const indiceAleatorio = Math.floor(Math.random() * allcities.length);
   //const campoAleatorio = allcities[indiceAleatorio];
-  console.log('La locacion seleccionada es: ' + allcities[indiceAleatorio]);
-
+  const randomCity = allcities[indiceAleatorio]
+  
+const locacion = randomCity.city+', '+randomCity.country
+console.log('La locacion seleccionada es: ',locacion);
   //await page.click('label:has-text("Location")');
   await page.locator('label:has-text("Location")').clear()
-  await page.locator('label:has-text("Location")').fill(allcities[indiceAleatorio])
+  await page.locator('label:has-text("Location")').fill(locacion)
   await page.keyboard.press('Enter')
 
 
--- tests/test-Completo.spec.ts
+++ tests/test-Completo.spec.ts
@@ -103,6 +103,87 @@ console.log('Impresion de los checks antes del filtro: ',OurRatingCheck)
 
   //RESERVAR EL HOTEL MAS ECONOMINCO >200 Y CON ESTRELLAS SELECCIONADAS
 
+  const hotelToFind = cheapestHotel; // El hotel que deseas buscar
+
+  const foundHotel = FilterHotelList.find(hotel => hotel.hotelName === hotelToFind.hotelName);
+  
+  if (foundHotel) {
+    console.log('El hotel ha sido encontrado:', foundHotel);
+  } else {
+    console.log('El hotel no fue encontrado en FilterHotelList.');
+  }
+
+  const pageBook = foundHotel.currentPage
+  const dataview = await page.locator('#MainContentPlaceHolder_HotelsDataView_PGB').isVisible();
+  if (dataview === true) {
+  const LocatorcurrentPage = '.dxp-num.dxp-current';//este localizador esta repetido en HotelList.ts
+  const actual = await page.locator(LocatorcurrentPage).innerText()
+  if (pageBook.toString() === actual){
+    console.log('EN LA MISMA PAGINA')
+    await page.waitForSelector('#MainContentPlaceHolder_HotelsDataView_ICell > table > tbody')
+    //await page.getByRole('link', { name: foundHotel.hotelName }).click();
+    //await page.getByRole('gridcell', { name: new RegExp(foundHotel.hotelName) }).getByText('Book It').click();
+   // await page.locator(`[name*="${foundHotel.hotelName}"]`).sibling('span').withText('Book It').click();
+   const locatorBookItHotel = "//a[contains(@class, 'dxeHyperlink_Metropolis hotel-title') and contains(text(), '"+foundHotel.hotelName+"')]/ancestor::div/following-sibling::div//span[contains(text(), 'Book It')]"
+   await page.locator(locatorBookItHotel).click()
+
+   await page.getByRole('cell', { name:new RegExp(foundHotel.hotelName)}).getByRole('button', { name: 'Book It' })
+    //await page.getByRole('row', { name: 'First Prev 1 2 3 Next Last', exact: true }).getByText(pageBook.toString()).click()
+    //hacer el click en Book It de es hotel en especifico foundHotel.hotelName
+  }else{
+    console.log('EN EL CAMBIO DE PAGINA')
+    await page.waitForSelector('#MainContentPlaceHolder_HotelsDataView_PGB')
+    await page.waitForSelector('#MainContentPlaceHolder_HotelsDataView_ICell > table > tbody')
+    const pageBookStr = foundHotel.currentPage.toString()
+    await page.locator("//a[contains(@class, 'dxp-num') and contains(text(), '"+foundHotel.currentPage.toString()+"')]").click()
+    //await page.getByText(pageBook.toString()).click()
+    //await page.getByRole('link', { name: pageBook.toString() }).getByText(pageBook.toString()).click()
+    //await page.getByRole('row', { name: new RegExp('First Prev')}).getByText(pageBook.toString()).click()
+   // await page.getByRole('link', { name: pageBookStr, exact: true }).getByText(pageBookStr).click()
+   await page.waitForSelector('#MainContentPlaceHolder_HotelsDataView_ICell > table > tbody')
+   const locatorBookItHotel = "//a[contains(@class, 'dxeHyperlink_Metropolis hotel-title') and contains(text(), '"+foundHotel.hotelName+"')]/ancestor::div/following-sibling::div//span[contains(text(), 'Book It')]"
+   await page.locator(locatorBookItHotel).click()
+   
+
+   
+   
+   await page.getByRole('cell', { name:new RegExp(foundHotel.hotelName)}).getByRole('button', { name: 'Book It' })
+
+/* 
+
+   1) [chromium] › test-Completo.spec.ts:9:5 › test ─────────────────────────────────────────────────
+
+   Error: locator.click: Error: strict mode violation: getByText('Book It') resolved to 4 elements:
+       1) <input type="submit" class="dxb-hb" value="Book It" id=…/> aka getByRole('cell', { name: 'The Grand Resort 5772 1st Street San Francisco, CA 94104, USA TYPE: Platinum LOCATION: AAA RATING: 5 $499.99 per night Book It Book It When opulance is your desire and when you only want the best in dining, shopping and guest room options, then choose the Grant Resort in downtown San Francisco. We cater to both the business and leisure traveller so please join us on your next visit.', exact: true }).getByRole('button', { name: 'Book It' })
+       2) <span class="dx-vam">Book It</span> aka locator('#MainContentPlaceHolder_HotelsDataView_IT0_BookItButton_0_CD span')
+       3) <input type="submit" class="dxb-hb" value="Book It" id=…/> aka getByRole('cell', { name: 'Golden Gate Hotel 6222 Bridge Rd San Francisco, CA 94102, USA TYPE: Diamond LOCATION: AA RATING: 4.5 $199.99 per night Book It Book It At the Golden Gate, we welcome you to the Golden State with open arms and strive to make your stay in San Francisco a memorable one. We wil do everything we can to make sure you have fun in the city and to earn your business year after year. Stay with us.', exact: true }).getByRole('button', { name: 'Book It' })
+       4) <span class="dx-vam">Book It</span> aka locator('#MainContentPlaceHolder_HotelsDataView_IT1_BookItButton_1_CD span')
+
+   =========================== logs ===========================
+   waiting for getByText('Book It')
+   ============================================================
+
+
+
+
+
+
+
+   await page.getByText('Book It').click()
+   
+
+ */
+
+
+   //await page.getByRole('gridcell', { name: new RegExp(foundHotel.hotelName) }).getByText('Book It', {exact: true}).click();
+
+    //await page.getByRole('gridcell', { name:foundHotel.hotelName, exact:false}).getByText('Book It').click()
+    //await page.getByRole('link', { name: foundHotel.hotelName }).click();
+    //hacer click en pageBook
+    //hacer el click en Book It de es hotel en especifico foundHotel.hotelName
+  }
+}
+
 
 
 
