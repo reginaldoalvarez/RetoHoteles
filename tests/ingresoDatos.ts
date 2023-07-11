78ab42b0d9bf780f9ee56f93bf1eab84e5bd473a
reginaldoalvarez <reginaldo.alvarez@gmail.com>
2023-07-10 20:37:13 -0500

    borrar archivos innecesarios

--- tests/ingresoDatos.ts
+++ tests/ingresoDatos.ts
@@ -1,64 +1,99 @@
 //ingresoDatos.ts
 export async function dataIN(page: any) {
-    // ... código de la función navigateAndFilterHotels ...
+  // ... código de la función navigateAndFilterHotels ...
+  let city: string; // Location
+  let addcity = true;
+  let allcities: { city: string; country: string; numHoteles: string }[] = [];
+  let temp = 0
+  //await page.click('label:has-text("Location")');
+  await page.locator('#MainContentPlaceHolder_SearchPanel_SearchPanelLayout_LocationComboBox_B-1').click();
+  //await page.press('body', 'ArrowDown');
+  const LocatorAllCities = '#MainContentPlaceHolder_SearchPanel_SearchPanelLayout_LocationComboBox_DDD_L_LBT.dxeListBoxItemRow_Metropolis';
+  await page.locator('LocatorAllCities')
+  //await page.waitForSelector(LocatorAllCities);
+  await page.waitForLoadState('load');
 
+  while (addcity) {
+    const tableElement = await page.$('#MainContentPlaceHolder_SearchPanel_SearchPanelLayout_LocationComboBox_DDD_L_LBT');
+    const rows = await tableElement?.$$('tr.dxeListBoxItemRow_Metropolis');
+    if (rows) {
+      let newCitiesAdded = false; // Variable para verificar si se han agregado nuevas ciudades en el ciclo
 
-let city: string; // Location
-let addcity = true;
-await page.click('label:has-text("Location")');
-await page.locator('#MainContentPlaceHolder_SearchPanel_SearchPanelLayout_LocationComboBox_B-1').click()
-//await page.press('body', 'ArrowDown');
-const LocatorAllCities = '#MainContentPlaceHolder_SearchPanel_SearchPanelLayout_LocationComboBox_DDD_L_LBT.dxeListBoxItemRow_Metropolis'
+      for (const row of rows) {
+        const cityElement = await row.$('td.dxeFTM');
+        const countryElement = await row.$('td.dxeTM');
+        const numHotelesElement = await row.$('td.dxeLTM');
 
-//await page.waitForSelector(LocatorAllCities)
-await page.waitForLoadState('load')
+        const city = await cityElement?.innerText();
+        const country = await countryElement?.innerText();
+        const numHoteles = await numHotelesElement?.innerText();
 
-const allcities: { city: string; country: string; numHoteles: string }[] = [];
+        if (city && country && numHoteles) {
+          if (!allcities.some(item => item.city === city)) {
+            allcities.push({ city, country, numHoteles });
+            newCitiesAdded = true; // Se ha agregado una nueva ciudad
+          }
+        }
+      }
 
-const tableElement = await page.$('#MainContentPlaceHolder_SearchPanel_SearchPanelLayout_LocationComboBox_DDD_L_LBT');
-const rows = await tableElement?.$$('tr.dxeListBoxItemRow_Metropolis');
+      if (!newCitiesAdded) {
+        // No se encontraron nuevas ciudades, se sale del bucle
 
-if (rows) {
-  for (const row of rows) {
-    const cityElement = await row.$('td.dxeFTM');
-    const countryElement = await row.$('td.dxeTM');
-    const numHotelesElement = await row.$('td.dxeLTM');
-
-    const city = await cityElement?.innerText();
-    const country = await countryElement?.innerText();
-    const numHoteles = await numHotelesElement?.innerText();
-
-    if (city && country && numHoteles) {
-      allcities.push({ city, country, numHoteles });
+        if (temp > 1) {
+          addcity = false;
+        }
+        temp++
+      }
     }
-  }
-}
 
-/* while (addcity) {
-  await page.click('label:has-text("Location")');
-  await page.press('body', 'ArrowDown');
-  city = await page.getByLabel('location').inputValue();
+    await page.press('body', 'PageDown');
+    await page.locator('LocatorAllCities')
+   // await page.waitForTimeout(1000); // Puedes ajustar el tiempo de espera según sea necesario
+  }
+
+
+
+
+
+
+
+  // Realizar scroll para cargar más contenido
+
+  //const firstElement = await page.$('td.dxeFTM');
+
+  // await page.locator('#MainContentPlaceHolder_SearchPanel_SearchPanelLayout_LocationComboBox_DDD_L_D').hover();
+
+
+
+  // Esperar un tiempo para que se carguen más países
+  //await page.waitForTimeout(2000); // Puedes ajustar el tiempo de espera según sea necesario
+
+
+  /* while (addcity) {
+    await page.click('label:has-text("Location")');
+    await page.press('body', 'ArrowDown');
+    city = await page.getByLabel('location').inputValue();
+    
+    const numHoteles = await page.locator('your-locator-for-counting-hotels').count().then(count => count.toString());
   
-  const numHoteles = await page.locator('your-locator-for-counting-hotels').count().then(count => count.toString());
+    if (!allcities.some(item => item.city === city)) {
+      allcities.push({ city, numHoteles });
+      addcity = true;
+    } else {
+      addcity = false;
+    }
+  } */
 
-  if (!allcities.some(item => item.city === city)) {
-    allcities.push({ city, numHoteles });
-    addcity = true;
-  } else {
-    addcity = false;
-  }
-} */
-
-  console.log('El total de Locaciones seleccionables son: ' + allcities.length);
+  console.log('El total de Locaciones seleccionables son: ', allcities.length);
   console.log(allcities);
 
 
   const indiceAleatorio = Math.floor(Math.random() * allcities.length);
   //const campoAleatorio = allcities[indiceAleatorio];
   const randomCity = allcities[indiceAleatorio]
-  
-const locacion = randomCity.city+', '+randomCity.country
-console.log('La locacion seleccionada es: ',locacion);
+
+  const locacion = randomCity.city + ', ' + randomCity.country
+  console.log('La locacion seleccionada es: ', locacion);
   //await page.click('label:has-text("Location")');
   await page.locator('label:has-text("Location")').clear()
   await page.locator('label:has-text("Location")').fill(locacion)
@@ -99,4 +134,4 @@ console.log('La locacion seleccionada es: ',locacion);
   await page.locator('label:has-text("Rooms")').fill(Rooms)
   await page.locator('label:has-text("Adults")').fill(Adults)
   await page.locator('label:has-text("Children")').fill(Children)
-  }
+}
